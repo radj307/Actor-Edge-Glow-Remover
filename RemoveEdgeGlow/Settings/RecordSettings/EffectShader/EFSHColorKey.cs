757e9e9d7f1b2a8013cf2c6e4cf4ca57515115fd
radj307 <radj@hotmail.ca>
2021-09-23 17:59:47 -0400

    Re-add color key settings in new format

---
+++ RemoveEdgeGlow/Settings/RecordSettings/EffectShader/EFSHColorKey.cs
@@ -0,0 +1,35 @@
+using System.Drawing;
+
+namespace RemoveEdgeGlow.SettingsObjects
+{
+    public class EFSHColorKey
+    {
+        public EFSHColorKey(bool enabled = true, int red = 0, int green = 0, int blue = 0, float time = 1.0f)
+        {
+            Enabled = enabled;
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Time = time;
+        }
+
+        public bool Enabled;
+        public int Red;
+        public int Green;
+        public int Blue;
+        public float Time;
+
+        private static float GetFloat(float current, float setting, out bool changed)
+        {
+            changed = !current.Equals(setting);
+            return changed ? setting : current;
+        }
+
+        public float GetTime(float current, out bool changed)
+        {
+            return GetFloat(current, Time, out changed);
+        }
+
+        public static implicit operator Color(EFSHColorKey obj) => Color.FromArgb(obj.Red, obj.Green, obj.Blue);
+    }
+}
