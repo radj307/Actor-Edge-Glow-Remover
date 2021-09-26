b8bc975e47435fdb4d472921f348258723951afe
radj307 <radj@hotmail.ca>
2021-09-26 00:03:51 -0400

    Add EditorID Common Name Whitelist & Blacklist

--- RemoveEdgeGlow/Settings/GenericUtils/MatchEditorID.cs
+++ RemoveEdgeGlow/Settings/GenericUtils/MatchEditorID.cs
@@ -1,67 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Mutagen.Bethesda.Plugins.Records;
 using Mutagen.Bethesda.WPF.Reflection.Attributes;
 
 namespace RemoveEdgeGlow.Settings.GenericUtils
 {
     public class MatchEditorID
     {
-        public MatchEditorID()
+        public MatchEditorID(List<string>? whitelist = null, List<string>? blacklist = null)
         {
-            InvalidEntryCount = 0;
-            CommonNames = new();
-        }
-        public MatchEditorID(List<string> commonNames)
-        {
-            InvalidEntryCount = commonNames.RemoveAll(n => n.Length < 2);
-            CommonNames = commonNames;
+            StringWhitelist = whitelist ?? new();
+            StringBlacklist = blacklist ?? new();
         }
 
         [MaintainOrder]
-        private bool Initialized; // used to remove any invalid user-added entries
-        private int InvalidEntryCount;
-        [Tooltip("If the Editor ID of a record contains a word included here, it will be considered whitelisted. Entries are Case-Insensitive and must be at least 2 characters long.")]
-        public List<string> CommonNames;
+        [SettingName("Common Names (Whitelist)")]
+        [Tooltip("Any records whose Editor ID contains one of these words will be added to the Whitelist")]
+        public List<string> StringWhitelist;
+        [SettingName("Common Names (Blacklist)")]
+        [Tooltip("Any records whose Editor ID contains one of these words will be added to the Blacklist")]
+        public List<string> StringBlacklist;
 
-        public bool GetInvalidEntryCount(out int invalidEntries)
+        protected bool IsWhitelistedEditorID(string editorID)
         {
-            if (!Initialized)
-                CommonNames = Validate(CommonNames, out InvalidEntryCount);
-            invalidEntries = InvalidEntryCount;
-            return invalidEntries > 0;
+            return StringWhitelist.Any(substr => editorID.Contains(substr, StringComparison.OrdinalIgnoreCase));
         }
 
-        /// <summary>
-        /// Remove short entries from the given string list.
-        /// </summary>
-        /// <param name="list">List of strings to validate.</param>
-        /// <param name="invalid">Number of invalid entries removed.</param>
-        /// <param name="rmShorterThan">Any list entries shorter or equal to this number are removed.</param>
-        /// <returns>List\<string\></returns>
-        private static List<string> Validate(List<string> list, out int invalid, int rmShorterThan = 2)
+        protected bool IsBlacklistedEditorID(string editorID)
         {
-            invalid = list.RemoveAll(n => n.Length < rmShorterThan);
-            return list;
-        }
-
-        /// <summary>
-        /// Check if the given IMajorRecordCommonGetter instance's editor ID contains a match.
-        /// </summary>
-        /// <typeparam name="T">Type that implements the IMajorRecordCommonGetter interface.</typeparam>
-        /// <param name="obj">An IMajorRecordCommonGetter instance.</param>
-        /// <returns>bool</returns>
-        public bool HasMatch<T>(T obj) where T : class, IMajorRecordCommonGetter
-        {
-            if (!Initialized)
-            {
-                CommonNames = Validate(CommonNames, out InvalidEntryCount);
-                Initialized = true;
-            }
-            if (obj.EditorID == null)
-                return false;
-            return CommonNames.Any(n => obj.EditorID.Contains(n, StringComparison.OrdinalIgnoreCase));
+            return StringBlacklist.Any(substr => editorID.Contains(substr, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
